f6230e53b0a64f0913a250de8f2a369ba4c56778
Sam <iaiznura@gmail.com>
2023-07-04 22:08:46 +0300

    done homework 1

---
+++ lms/views/lesson.py
@@ -0,0 +1,27 @@
+from rest_framework.generics import RetrieveAPIView, DestroyAPIView, ListAPIView, UpdateAPIView, CreateAPIView
+
+from lms.models import Lesson
+from lms.serrializers.lesson import LessonSerializer
+
+
+class LessonDetailView(RetrieveAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonListView(ListAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+class LessonCreateView(CreateAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+class LessonUpdateView(UpdateAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+class LessonDeleteView(DestroyAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+    
