c4cbade169204adc9eacca42179d6c4e7cbff988
Sam <iaiznura@gmail.com>
2023-07-28 23:52:09 +0300

    add celery task in course update view

--- lms/views/course.py
+++ lms/views/course.py
@@ -1,12 +1,25 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
-from lms.models import Course
+from lms.models import Course, Subscription
 from lms.permissions import OwnerOrStuff
 from lms.serrializers.course import CourseSerializer
+from lms.services.mailing_subscription import mailing_by_subscriptions
+from lms.tasks import mailing_by_update_course
 
 
 class CourseViewSet(ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
     permission_classes = [IsAuthenticated, OwnerOrStuff]
+
+    def update(self, request, *args, **kwargs):
+        course_id = self.kwargs['pk']
+        course = get_object_or_404(Course, id=course_id)
+        subscriptions = Subscription.objects.filter(course=course)
+        if course.is_active:
+            for subscription in subscriptions:
+                email = subscription.user.email
+                mailing_by_update_course.delay(email, course=course.title)
+        return super().update(request, *args, **kwargs)
