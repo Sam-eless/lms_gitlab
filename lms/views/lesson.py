5c7c9e5b34053f8a00dd167f4713fdceb0113950
Sam <iaiznura@gmail.com>
2023-07-28 23:52:00 +0300

    add celery task in lesson update

--- lms/views/lesson.py
+++ lms/views/lesson.py
@@ -1,10 +1,14 @@
-from rest_framework.generics import RetrieveAPIView, DestroyAPIView, ListAPIView, UpdateAPIView, CreateAPIView
+from rest_framework.generics import RetrieveAPIView, DestroyAPIView, ListAPIView, UpdateAPIView, CreateAPIView, \
+    get_object_or_404
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-from lms.models import Lesson
+from lms.models import Lesson, Course, Subscription
 from lms.pagination import CustomPagination
 from lms.permissions import OwnerOrStuff
 from lms.serrializers.lesson import LessonSerializer
+from lms.services.mailing_subscription import mailing_by_subscriptions
+from lms.tasks import mailing_by_update_course
 
 
 class LessonDetailView(RetrieveAPIView):
@@ -34,6 +38,20 @@ class LessonUpdateView(UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
     permission_classes = [IsAuthenticated, OwnerOrStuff]
+
+    def partial_update(self, request, *args, **kwargs):
+        response = request.data
+
+        lesson_id = self.kwargs['pk']
+        lesson = get_object_or_404(Lesson, id=lesson_id)
+        course = get_object_or_404(Course, id=lesson.course_id)
+
+        subscriptions = Subscription.objects.filter(course=course)
+        if lesson.is_active and course.is_active:
+            for subscription in subscriptions:
+                email = subscription.user.email
+                mailing_by_update_course.delay(email, lesson=lesson.title, course=course.title)
+        return super().partial_update(request, *args, **kwargs)
 
 
 class LessonDeleteView(DestroyAPIView):
