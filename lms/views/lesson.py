39f61a8ff6011c2b2d29f3b8d92c7b1f760265ef
Sam <iaiznura@gmail.com>
2023-07-29 17:06:02 +0300

    add celery task in lesson create

--- lms/views/lesson.py
+++ lms/views/lesson.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.generics import RetrieveAPIView, DestroyAPIView, ListAPIView, UpdateAPIView, CreateAPIView, \
     get_object_or_404
 from rest_framework.permissions import IsAuthenticated
@@ -29,9 +30,19 @@ class LessonCreateView(CreateAPIView):
     queryset = Lesson.objects.all()
 
     permission_classes = [IsAuthenticated, OwnerOrStuff]
-    # def create(self, request, *args, **kwargs):
-    #     print(request.data)  # Проверяем данные из запроса
-    #     return super().create(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        super().create(request, *args, **kwargs)
+        lesson_title = self.request.data['title']
+        lesson = get_object_or_404(Lesson, title=lesson_title)
+        course = get_object_or_404(Course, id=lesson.course_id)
+
+        subscriptions = Subscription.objects.filter(course=course)
+        if lesson.is_active and course.is_active:
+            for subscription in subscriptions:
+                email = subscription.user.email
+                mailing_by_update_course.delay(email, lesson=lesson.title, course=course.title)
+        return Response(request.data, status=status.HTTP_201_CREATED)
 
 
 class LessonUpdateView(UpdateAPIView):
@@ -40,7 +51,6 @@ class LessonUpdateView(UpdateAPIView):
     permission_classes = [IsAuthenticated, OwnerOrStuff]
 
     def partial_update(self, request, *args, **kwargs):
-        response = request.data
 
         lesson_id = self.kwargs['pk']
         lesson = get_object_or_404(Lesson, id=lesson_id)
