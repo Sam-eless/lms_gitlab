9038f49a50f8591c2d8bf1cc1f43561779f3277d
Sam <iaiznura@gmail.com>
2023-07-29 19:35:20 +0300

    add task check_last_login_user

--- lms/tasks.py
+++ lms/tasks.py
@@ -1,8 +1,14 @@
 from celery import shared_task
 
-from lms.services.mailing_subscription import mailing_by_subscriptions
+from lms.services.services import mailing_by_subscriptions, deactivation_user_after_month
+from users.models import User
 
 
 @shared_task
 def mailing_by_update_course(email, course, lesson=None):
     mailing_by_subscriptions(email, course, lesson)
+
+
+@shared_task
+def check_last_login_user():
+    deactivation_user_after_month()
