388dcde89f7e02ca69c34e52070cc0aa7213cf30
Sam <iaiznura@gmail.com>
2023-08-27 16:03:37 +0000

    Update csu.py

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -6,14 +6,14 @@ from users.models import User
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        if not User.objects.get(email="admin@sky.pro"):
+            user = User.objects.create(
+                email='admin@sky.pro',
+                first_name='admin',
+                last_name='admin',
+                is_staff=True,
+                is_superuser=True
+            )
 
-        user = User.objects.create(
-            email='admin@sky.pro',
-            first_name='admin',
-            last_name='admin',
-            is_staff=True,
-            is_superuser=True
-        )
-
-        user.set_password('123131')
-        user.save()
+            user.set_password('123131')
+            user.save()
